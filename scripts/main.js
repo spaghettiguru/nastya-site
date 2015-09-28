85ff4825bf77d431c76d9750c7260fcbf2b81004
spaghettiguru <yuzvik.oleg@gmail.com>
2015-09-29 00:52:24 +0300

    navigation and typography

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,33 @@
-var canvasCtx = document.querySelector(".projector-light").getContext("2d");
+var canvas = document.querySelector(".projector-light");
+var canvasCtx = canvas.getContext("2d");
 var screen = document.querySelector(".main-content");
-var screenBB = screen.getBoundingClientRect();
-var menuItems = document.querySelectorAll(".nav-link");
 
-var firstItemBB = menuItems[1].getBoundingClientRect();
-canvasCtx.beginPath();
-canvasCtx.moveTo(firstItemBB.right - 20, firstItemBB.top - 20 + firstItemBB.height/2 );
-canvasCtx.lineTo(screenBB.left - 20, screenBB.top - 20);
-canvasCtx.lineTo(screenBB.left - 20, screenBB.bottom - 20);
-canvasCtx.closePath();
-canvasCtx.fillStyle = "rgba(255, 255, 255, .12)";
-canvasCtx.fill();
+function findMenuItemByURLHash(hash) {
+	return document.querySelector(".nav-link[href=\"" + hash + "\"]");
+}
+
+function drawLightFromMenuItem(menuItem) {
+	if (menuItem) {
+		var screenBB = screen.getBoundingClientRect();
+		var menuItemBB = menuItem.getBoundingClientRect();
+		canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+		canvasCtx.beginPath();
+		canvasCtx.moveTo(menuItemBB.right - 10, menuItemBB.top - 20 + menuItemBB.height/2 );
+		canvasCtx.lineTo(screenBB.left - 20, screenBB.top - 20);
+		canvasCtx.lineTo(screenBB.left - 20, screenBB.bottom - 20);
+		canvasCtx.closePath();
+		canvasCtx.fillStyle = "rgba(255, 255, 255, .12)";
+		canvasCtx.fill();
+	}
+}
+
+drawLightFromMenuItem(findMenuItemByURLHash("#mymotherlovesme"));
+
+window.addEventListener("hashchange", function(event) {
+	var menuItem = findMenuItemByURLHash(window.location.hash);
+
+	var oldSelectedItem = document.querySelector(".nav-selected");
+	oldSelectedItem.classList.remove("nav-selected");
+	menuItem.classList.add("nav-selected");
+	drawLightFromMenuItem(menuItem);
+});
